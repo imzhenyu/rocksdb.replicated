be132439ce8815444d507f48ce6292edc18e43b8
Zhenyu Guo <imzhenyu@outlook.com>
2015-04-27 12:46:14 +0800

    refine response type, add link libraries

--- rdsn/rrdb.thrift
+++ rdsn/rrdb.thrift
@@ -9,12 +9,18 @@ struct update_request
     2:dsn.blob      value;
 }
 
+struct read_response
+{
+    1:int      error;
+    2:dsn.blob value;
+}
+
 service rrdb
 {
     int put(1:update_request update);
     int remove(1:dsn.blob key);
     int merge(1:update_request update);
-    dsn.blob get(1:dsn.blob key);
+    read_response get(1:dsn.blob key);
 }
 
 
