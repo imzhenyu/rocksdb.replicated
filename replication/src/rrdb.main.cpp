b461c5378940c2301d0a30928a3c2cdb522f7406
Zhenyu Guo <imzhenyu@outlook.com>
2015-07-14 12:53:03 +0800

    update with the latest codegen in rDSN

--- replication/src/rrdb.main.cpp
+++ replication/src/rrdb.main.cpp
@@ -11,22 +11,23 @@
 
 int main(int argc, char** argv)
 {
-	// register replication application provider
+    // register replication application provider
     dsn::replication::register_replica_provider<::dsn::apps::rrdb_service_impl>("rrdb");
 
     // register all possible services
     dsn::service::system::register_service<::dsn::replication::meta_service_app>("meta");
     dsn::service::system::register_service<::dsn::replication::replication_service_app>("replica");
-	dsn::service::system::register_service<::dsn::apps::rrdb_client_app>("client");
+    dsn::service::system::register_service<::dsn::apps::rrdb_client_app>("client");
+    dsn::service::system::register_service<::dsn::apps::rrdb_perf_test_client_app>("client.rrdb.perf.test");
 
-	// register all possible tools and toollets
-	dsn::tools::register_tool<dsn::tools::nativerun>("nativerun");
-	dsn::tools::register_tool<dsn::tools::simulator>("simulator");
-	dsn::tools::register_toollet<dsn::tools::tracer>("tracer");
-	dsn::tools::register_toollet<dsn::tools::profiler>("profiler");
-	dsn::tools::register_toollet<dsn::tools::fault_injector>("fault_injector");
+    // register all possible tools and toollets
+    dsn::tools::register_tool<dsn::tools::nativerun>("nativerun");
+    dsn::tools::register_tool<dsn::tools::simulator>("simulator");
+    dsn::tools::register_toollet<dsn::tools::tracer>("tracer");
+    dsn::tools::register_toollet<dsn::tools::profiler>("profiler");
+    dsn::tools::register_toollet<dsn::tools::fault_injector>("fault_injector");
 
-	// specify what services and tools will run in config file, then run
-	dsn::service::system::run(--argc, ++argv, true);
-	return 0;
+    // specify what services and tools will run in config file, then run
+    dsn::service::system::run(--argc, ++argv, true);
+    return 0;
 }
